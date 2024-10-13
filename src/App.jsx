08458ae9c6844ad9591a6e3dc970a6593baa666f
Tom <45665177+TomTom2019@users.noreply.github.com>
2024-10-13 15:38:07 +0200

    mainsrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const title = 'React'
 
 function App() {
 // you can do something in between
@@ -10,7 +7,7 @@ function App() {
 	
 return (
 <div>
-<h1>Hello React</h1>
+<h1>Hello {title} </h1>
 </div>
 );
 }
