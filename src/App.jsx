b6e81439c99d9283ff50f183dc764637eb773427
Tom <45665177+TomTom2019@users.noreply.github.com>
2024-10-18 16:43:53 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <Search />
 
-      <Search />
+    <hr />
 
-      <hr />
-
-      <List list={stories} />
-    </div>
-  );
-};
+    <List />
+  </div>
+);
 
 const Search = () => {
   const handleChange = (event) => {
@@ -48,23 +45,19 @@ const Search = () => {
   );
 };
 
-const List = (props) => (
+const List = () => (
   <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
     ))}
   </ul>
-);
-
-const Item = (props) => (
-  <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
 );
 
 export default App;
