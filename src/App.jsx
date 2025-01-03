8de66beb7308d2d31da5c96d2f71327c7a4b91fd
Tom <45665177+TomTom2019@users.noreply.github.com>
2025-01-03 11:45:54 +0100

    upp

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
-function getTitle(title){
-  return title
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App()  {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+
+      <hr />
+
+      <List />
+    </div>
+  );
 }
 
 
-
-
-function App(){
-  return(
-   <div>
-     <h1>hello {getTitle('React')} </h1>
-
-   <label htmlFor="search">Search</label>
-   <input id="search" type="text"/>
-
-
-   </div>
-
-
-    )
+//// SEARCH
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
 }
 
-export default App
+////LIST
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+export default App;
