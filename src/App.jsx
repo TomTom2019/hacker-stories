878c18ba5e0090dde24f785fd7db9b54c6f79cff
Tom <45665177+TomTom2019@users.noreply.github.com>
2025-01-05 15:34:12 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import * as React from 'react';
+
+
 const list = [
   {
     title: 'React',
@@ -17,7 +20,32 @@ const list = [
   },
 ];
 
-function App()  {
+function App() {
+  console.log('App renders')
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID:0,
+    },
+   {
+     title: 'Redux',
+     url: 'https://redux.js.org/',
+     author: 'Dan Abramov, Andrew Clark',
+     num_comments: 2,
+     points: 5,
+     objectID:1,
+   },
+  ]
+
+
+
+
+
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -26,36 +54,55 @@ function App()  {
 
       <hr />
 
-      <List />
+      <List list={stories} />
     </div>
   );
 }
 
 
 //// SEARCH
-function Search() {
+const Search = () =>{
+ const [searchTerm, setSearchTerm]= React.useState('')
+
+
+   const handleChange = (event)=>{
+    setSearchTerm(event.target.value) 
+   }
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>Searching for <strong>{searchTerm} </strong></p>
     </div>
   );
 }
 
 ////LIST
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
-    ))}
+   {props.list.map((item)=>(
+       <Item key={item.objectID} item={item} />
+    ))} 
   </ul>
 );
+
+const Item = (props) => (
+
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+
+  </li>
+
+
+)
+
+
 
 export default App;
