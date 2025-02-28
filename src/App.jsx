5f2f713bd2c80eae2c2726833a537446b1d3e3a1
Tom <45665177+TomTom2019@users.noreply.github.com>
2025-02-28 14:29:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const list = [
   {
     title: 'React',
-    url: 'https://react.dev/',
+    url: 'https://reactjs.org/',
     author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
@@ -14,39 +14,41 @@ const list = [
     num_comments: 2,
     points: 5,
     objectID: 1,
-  }
-]
+  },
+];
 
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-function App() {
-  
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <Search />
 
-      <label htmlFor="search">Search:</label>
-      <input id='search' type="text" />
- 
-       <hr/>
+    <hr />
 
-        <ul>
-          
-          {list.map(function (item){
-            return (
-              <li key={item.objectID} >
-                <span>
-                  <a href={item.url} >{item.title} </a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </li>
-            )
-          })}
-      </ul>
-    </div>
-      
-  )
-}
+    <List />
+  </div>
+);
 
-export default App
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+export default App;
